2d5ceec6f141be3bb301c3d3296675cd5e32f92b
yukinea <yuki.931322@gmail.com>
2019-03-04 01:38:42 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::KemonoFriends::Color - It's new $module
+
+# SYNOPSIS
+
+    use Acme::KemonoFriends::Color;
+
+# DESCRIPTION
+
+Acme::KemonoFriends::Color is ...
+
+# LICENSE
+
+Copyright (C) yukinea.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+yukinea <yuki.931322@gmail.com>
---
+++ lib/Acme/KemonoFriends/Color.pm
@@ -0,0 +1,39 @@
+package Acme::KemonoFriends::Color;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::KemonoFriends::Color - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::KemonoFriends::Color;
+
+=head1 DESCRIPTION
+
+Acme::KemonoFriends::Color is ...
+
+=head1 LICENSE
+
+Copyright (C) yukinea.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+yukinea E<lt>yuki.931322@gmail.comE<gt>
+
+=cut
+
